4aeb2714a5505acd666b15121b19f87e6954341e
Kasper Balink <kasperbalink@hotmail.com>
2014-12-15 12:01:07 +0100

    Geolocation added

---
+++ TripViewBreda/TripViewBreda/GeoLocation/GPS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace TripViewBreda.GeoLocation
+{
+    class GPS
+    {
+        Geopoint myPoint;
+        private async void UpdateCurrentLocation()
+        {
+            var locator = new Geolocator();
+            locator.DesiredAccuracyInMeters = 50;
+
+            var position = await locator.GetGeopositionAsync();
+            myPoint = position.Coordinate.Point;
+        }
+        public GPSPoint getCurrentLocation()
+        {
+            UpdateCurrentLocation();
+            return new GPSPoint(myPoint.Position.Latitude, myPoint.Position.Longitude);
+        }
+    }
+}
---
+++ TripViewBreda/TripViewBreda/GeoLocation/GPSPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripViewBreda.GeoLocation
+{
+    class GPSPoint
+    {
+        //OVERBODIG????
+        //
+        //
+        private double lattitude;
+        private double longitude;
+
+        public GPSPoint(double lattitude, double longitude)
+        {
+            this.lattitude = lattitude;
+            this.longitude = longitude;
+        }
+
+        public double GetLattitude()
+        {
+            return this.lattitude;
+        }
+        public double GetLongitude()
+        {
+            return this.longitude;
+        }
+        
+    }
+}
